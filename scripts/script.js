d2c9c2cdbcd1db1cf74fedfe48aba8f382f5cb9f
Juliusmeeuwisse <74155177+Juliusmeeuwisse@users.noreply.github.com>
2021-01-14 22:21:53 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+var Nav = document.getElementById("hamburgermenu")
+
+function openNav() {
+  hamburgermenu.style.width = "100%";
+}
+
+function closeNav() {
+  hamburgermenu.style.width = "0%";
+}
